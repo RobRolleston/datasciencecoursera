b70b098bac79098108614794baa133dd686ba3b5
RobRolleston <rob.rolleston@xerox.com>
2015-01-20 06:59:53 -0500

    Initial set up of scaffolding

---
+++ 02-R/wk-4/best.R
@@ -0,0 +1,8 @@
+best <- function(state, outcome) {
+	# Read the outcome data
+	
+	## Check that state and outcome are valid
+	
+	## Return hospital name in that state with the lowest 30-day death rate
+	
+}
